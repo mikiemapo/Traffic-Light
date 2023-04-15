1fdce4cb94e4f7c5dc55566bf0a0e3f4c6107d0a
mikiemapo <findmikemm@gmail.com>
2023-04-15 01:21:05 +0000

    use effect hook for timeout added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,60 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const TrafficLight = (props) => {
-  const [trafficColor, setTrafficColor] = useState('');
+const TrafficLight = () => {
+  const [trafficColor, setTrafficColor] = useState("");
 
+  useEffect(() => {
+    const redTimeout = setTimeout(() => {
+      setTrafficColor("red");
+    }, 1000);
+
+    const yellowTimeout = setTimeout(() => {
+      setTrafficColor("yellow");
+    }, 2000);
+
+    const greenTimeout = setTimeout(() => {
+      setTrafficColor("green");
+    }, 3000);
+
+    const clearAllTimeouts = () => {
+      clearTimeout(redTimeout);
+      clearTimeout(yellowTimeout);
+      clearTimeout(greenTimeout);
+    };
+    
+    const resetTimeout = setTimeout(() => {
+      setTrafficColor("");
+    }, 4000);
+
+    return () => {
+      clearAllTimeouts();
+      clearTimeout(resetTimeout);
+    };
+  }, []);
 
   return (
-    
-   <div className="pole">
+    <div className="pole">
+      <div className="blackbox">
+        <div
+          className={"light red " + (trafficColor == "red" ? "glowr" : "")}
+          onClick={() => setTrafficColor("red")}
+        ></div>
 
-    <div className="blackbox">
+        <div
+          className={
+            "light yellow " + (trafficColor == "yellow" ? "glowy" : "")
+          }
+          onClick={() => setTrafficColor("yellow")}
+        ></div>
 
-      <div className={'light red '+ (trafficColor == "red"? 'glowr': "")} onClick={() => setTrafficColor("red")}>
-      
+        <div
+          className={"light green " + (trafficColor == "green" ? "glowg" : "")}
+          onClick={() => setTrafficColor("green")}
+        ></div>
       </div>
-
-      <div className={'light yellow '+ (trafficColor == "yellow"? 'glowy': "")} onClick={() => setTrafficColor("yellow")}>
-        
-      </div>
-
-      <div className={'light green '+(trafficColor == "green"? 'glowg': "")} onClick={() => setTrafficColor("green")}>
-       
-      </div>
-
-      </div>
-
     </div>
   );
 };
 
 export default TrafficLight;
-
-// function App() {
-//   return (
-
-//     <div className="App">
-
-//     </div>
-//   );
-// }
-
-// export default App;
